92a9c301aba0f6a205d4b097f95109ad8c6d5ecd
Glaz8212 <lws7129@naver.com>
2024-08-21 15:20:58 +0900

    240821-ct

---
+++ Level.00/031.RemoveChar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace _031.RemoveChar
+{
+    internal class Program
+    {
+        // 2024.08.21
+        // 문자열 my_string과 문자 letter이 매개변수로 주어집니다.
+        // my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+        // https://school.programmers.co.kr/learn/courses/30/lessons/120826
+        public class Solution
+        {
+            public string solution(string my_string, string letter)
+            {
+                return my_string.Replace(letter, "");
+                // string.Replace(a,b);
+                // string내의 a를 b로 바꿈.
+            }
+        }
+    }
+}
---
+++ Level.00/034.SumOfEven/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _034.SumOfEven
+{
+    internal class Program
+    {
+        // 2024.08.21
+        // 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+        // https://school.programmers.co.kr/learn/courses/30/lessons/120831
+        public class Solution
+        {
+            public int solution(int n)
+            {
+                int answer = 0;
+                if ( n % 2 == 0 )
+                {
+                    answer = (1 + n / 2) * n / 2;
+                }
+                if (n % 2 == 1)
+                {
+                    answer = (1 + (n - 1) / 2) * (n - 1) / 2;
+                }
+                return answer;
+            }
+        }
+    }
+}

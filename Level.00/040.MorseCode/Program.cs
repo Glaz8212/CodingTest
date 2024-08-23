8605fca70374922540ab4926f8b295be9a78fb57
Glaz8212 <lws7129@naver.com>
2024-08-23 16:42:01 +0900

    240823-ct

---
+++ Level.00/040.MorseCode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace _040.MorseCode
+{
+    internal class Program
+    {
+        // 2024.08.23
+        // 머쓱이는 친구에게 모스부호를 이용한 편지를 받았습니다.
+        // 그냥은 읽을 수 없어 이를 해독하는 프로그램을 만들려고 합니다.
+        // 문자열 letter가 매개변수로 주어질 때, letter를 영어 소문자로 바꾼 문자열을 return 하도록
+        // solution 함수를 완성해보세요.
+        // 모스부호는 다음과 같습니다.
+        /* morse = { 
+                    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+                    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+                    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+                    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+                    '-.--':'y','--..':'z'
+                    } */
+        // https://school.programmers.co.kr/learn/courses/30/lessons/120838
+        public class Solution
+        {
+            // answer스트링빌더 만들고
+            // list로 모스부호 담아서,
+            // 입력한 모스부호 split 해서 list에서 찾고,
+            // 인덱스 번호에 + 해서 아스키
+            public string solution(string letter)
+            {
+                StringBuilder answer = new StringBuilder();
+                List<string> morse = new List<string>() { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+                string[] splitLetter = letter.Split(' ');
+
+                foreach (string str in splitLetter)
+                {
+                    int index = morse.IndexOf(str);
+                    answer.Append((char)('a' + index));
+                }
+                return answer.ToString();
+            }
+        }
+    }
+}
---
+++ Level.00/042.SplitBeads/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace _042.SplitBeads
+{
+    internal class Program
+    {
+        // 2024.08.23
+        // 머쓱이는 구슬을 친구들에게 나누어주려고 합니다.
+        // 구슬은 모두 다르게 생겼습니다.
+        // 머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때,
+        // balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+        // https://school.programmers.co.kr/learn/courses/30/lessons/120840
+        public class Solution
+        {
+            public int solution(int balls, int share)
+            {
+                BigInteger answer = Factorial(balls) / (Factorial(balls - share) * Factorial(share));
+                return (int)answer;
+            }
+
+            static BigInteger Factorial(int num)
+            {
+                BigInteger result = 1;
+                for (int i = 1; i <= num; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            }
+        }
+    }
+}
